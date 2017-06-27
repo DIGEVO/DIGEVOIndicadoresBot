02c5901cac4281d5f18a3be381ec16b6a782e97b
Rigoberto Leander Salgado Reyes <rlsalgado2006@@gmail.com>
2017-06-27 00:23:42 -0500

    testing luis

--- DIGEVOIndicadoresBot/Dialogs/IndicadoresLuisDialog.cs
+++ DIGEVOIndicadoresBot/Dialogs/IndicadoresLuisDialog.cs
@@ -2,6 +2,7 @@
 using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Luis.Models;
 using System;
+using System.Configuration;
 using System.Threading.Tasks;
 
 namespace DIGEVOIndicadoresBot.Dialogs
@@ -9,17 +10,10 @@ namespace DIGEVOIndicadoresBot.Dialogs
     [Serializable]
     public class IndicadoresLuisDialog : LuisDialog<object>
     {
-        //var modelId = ConfigurationManager.AppSettings.Get("LuisModelId");
-        //var subscriptionKey = ConfigurationManager.AppSettings.Get("LuisSubscriptionKey");
-        //public IndicadoresLuisDialog() : base(new LuisService(new LuisModelAttribute(Utils.GetAppSetting("LuisAppId"), Utils.GetAppSetting("LuisAPIKey"))))
-        //public IndicadoresLuisDialog() : base(new LuisService(new LuisModelAttribute("0bc42f76-e6dc-4716-befb-6b6dbcb537bd", "04e3a883c75341189f83ca05676255ac")))
         public IndicadoresLuisDialog() : base(
-            new LuisService(
-                new LuisModelAttribute(
-                    System.Configuration.ConfigurationManager.AppSettings["LuisAppId"],
-                    System.Configuration.ConfigurationManager.AppSettings["LuisAPIKey"])))
+            new LuisService(new LuisModelAttribute(ConfigurationManager.AppSettings["LuisAppId"],ConfigurationManager.AppSettings["LuisAPIKey"])))
         {
-            Console.WriteLine("probando");
+            //todo buscar la forma de realizar log.
         }
 
         [LuisIntent("None")]
@@ -30,13 +24,14 @@ namespace DIGEVOIndicadoresBot.Dialogs
             context.Wait(MessageReceived);
         }
 
-        //// Go to https://luis.ai and create a new intent, then train/publish your luis app.
-        //// Finally replace "MyIntent" with the name of your newly created intent in the following handler
-        //[LuisIntent("conocer")]
-        //public async Task MyIntent(IDialogContext context, LuisResult result)
-        //{
-        //    await context.PostAsync($"You have reached the MyIntent intent. You said: {result.Query}"); //
-        //    context.Wait(MessageReceived);
-        //}
+        // Go to https://luis.ai and create a new intent, then train/publish your luis app.
+        // Finally replace "MyIntent" with the name of your newly created intent in the following handler
+        [LuisIntent("conocer")]
+        public async Task Conocer(IDialogContext context, LuisResult result)
+        {
+            
+            await context.PostAsync($"You have reached the \"conocer\" intent. You said: {result.Query}, {result.Entities.ToString()}"); //
+            context.Wait(MessageReceived);
+        }
     }
 }
