601376178b5a6ebb77ac5e4cd06530806afa5675
Rigoberto Leander Salgado Reyes <rlsalgado2006@@gmail.com>
2017-06-27 00:02:35 -0500

    testing luis

--- DIGEVOIndicadoresBot/Dialogs/IndicadoresLuisDialog.cs
+++ DIGEVOIndicadoresBot/Dialogs/IndicadoresLuisDialog.cs
@@ -13,7 +13,8 @@ namespace DIGEVOIndicadoresBot.Dialogs
     {
         //var modelId = ConfigurationManager.AppSettings.Get("LuisModelId");
         //var subscriptionKey = ConfigurationManager.AppSettings.Get("LuisSubscriptionKey");
-        public IndicadoresLuisDialog() : base(new LuisService(new LuisModelAttribute(Utils.GetAppSetting("LuisAppId"), Utils.GetAppSetting("LuisAPIKey"))))
+        //public IndicadoresLuisDialog() : base(new LuisService(new LuisModelAttribute(Utils.GetAppSetting("LuisAppId"), Utils.GetAppSetting("LuisAPIKey"))))
+        public IndicadoresLuisDialog() : base(new LuisService(new LuisModelAttribute("0bc42f76-e6dc-4716-befb-6b6dbcb537bd", "04e3a883c75341189f83ca05676255ac")))
         //public IndicadoresLuisDialog() : base(
         //    new LuisService(
         //        new LuisModelAttribute(
@@ -31,13 +32,13 @@ namespace DIGEVOIndicadoresBot.Dialogs
             context.Wait(MessageReceived);
         }
 
-        // Go to https://luis.ai and create a new intent, then train/publish your luis app.
-        // Finally replace "MyIntent" with the name of your newly created intent in the following handler
-        [LuisIntent("conocer")]
-        public async Task MyIntent(IDialogContext context, LuisResult result)
-        {
-            await context.PostAsync($"You have reached the MyIntent intent. You said: {result.Query}"); //
-            context.Wait(MessageReceived);
-        }
+        //// Go to https://luis.ai and create a new intent, then train/publish your luis app.
+        //// Finally replace "MyIntent" with the name of your newly created intent in the following handler
+        //[LuisIntent("conocer")]
+        //public async Task MyIntent(IDialogContext context, LuisResult result)
+        //{
+        //    await context.PostAsync($"You have reached the MyIntent intent. You said: {result.Query}"); //
+        //    context.Wait(MessageReceived);
+        //}
     }
 }
