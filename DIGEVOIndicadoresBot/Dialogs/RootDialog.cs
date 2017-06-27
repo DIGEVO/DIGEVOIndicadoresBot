57ed8de999bd186e69f91e8b2581be3aaed3e720
Rigoberto Leander Salgado Reyes <rlsalgado2006@@gmail.com>
2017-06-26 23:26:12 -0500

    testing luis

--- DIGEVOIndicadoresBot/Dialogs/RootDialog.cs
+++ DIGEVOIndicadoresBot/Dialogs/RootDialog.cs
@@ -21,9 +21,9 @@ namespace DIGEVOIndicadoresBot.Dialogs
 
             // calculate something for us to return
             int length = (activity.Text ?? string.Empty).Length;
-
+            
             // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+            await context.PostAsync($"{activity.From.Name}, You sent {activity.Text} which was {length} characters");
 
             context.Wait(MessageReceivedAsync);
         }
