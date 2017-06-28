e27cafb7131bbf234da0ae31c9396f93121e59e1
Rigoberto Leander Salgado Reyes <rlsalgado2006@@gmail.com>
2017-06-27 22:12:06 -0500

    testing luis

--- DIGEVOIndicadoresBot/Dialogs/IndicadoresLuisDialog.cs
+++ DIGEVOIndicadoresBot/Dialogs/IndicadoresLuisDialog.cs
@@ -3,6 +3,7 @@ using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Luis.Models;
 using System;
 using System.Configuration;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,7 +13,7 @@ namespace DIGEVOIndicadoresBot.Dialogs
     public class IndicadoresLuisDialog : LuisDialog<object>
     {
         public IndicadoresLuisDialog() : base(
-            new LuisService(new LuisModelAttribute(ConfigurationManager.AppSettings["LuisAppId"],ConfigurationManager.AppSettings["LuisAPIKey"])))
+            new LuisService(new LuisModelAttribute(ConfigurationManager.AppSettings["LuisAppId"], ConfigurationManager.AppSettings["LuisAPIKey"])))
         {
             //todo buscar la forma de realizar log.
         }
@@ -21,17 +22,20 @@ namespace DIGEVOIndicadoresBot.Dialogs
         public async Task NoneIntent(IDialogContext context, LuisResult result)
         {
             //context.Activity
-            await context.PostAsync($"{context.Activity.From.Name}, You have reached the none intent. You said: {result.Query}"); //
+            //todo guardar cuando ya lo salude!
+            var meridian = context.Activity.LocalTimestamp.Value.ToString("tt", CultureInfo.InvariantCulture).ToLower();
+            var hour = context.Activity.LocalTimestamp.Value.Hour;
+            var greetings = meridian == "am" ? "buenos días" : hour >= 19 ? "buenas noches" : "buenas tardes";
+            await context.PostAsync($"Hola {context.Activity.From.Name}, {greetings}, disculpa ¿puedes repetir tu pregunta?: {result.Query}");
             context.Wait(MessageReceived);
         }
 
-        // Go to https://luis.ai and create a new intent, then train/publish your luis app.
-        // Finally replace "MyIntent" with the name of your newly created intent in the following handler
         [LuisIntent("conocer")]
-        public async Task Conocer(IDialogContext context, LuisResult result)
+        public async Task KnowIntent(IDialogContext context, LuisResult result)
         {
             var str = String.Join(", ", Enumerable.Select(result.Entities, e => $"{e.Entity.ToString()} {e.Type.ToString()} {e.EndIndex.ToString()}").ToList());
-            await context.PostAsync($"You have reached the \"conocer\" intent. You said: {result.Query}, {str}"); //
+            var str1 = String.Join(", ", Enumerable.SelectMany(result.CompositeEntities, ce => ce.Children.Select(cc => cc.Value)));
+            await context.PostAsync($"Hola {context.Activity.From.Name}, entiendo que deseas \"conocer\" acerca de: {result.Query}, {str} {str1}");
             context.Wait(MessageReceived);
         }
     }
